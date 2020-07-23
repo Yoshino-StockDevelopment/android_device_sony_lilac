52006a15958e47187e7a29625f8eeea848ef7baf
= <mmfshujath@gmail.com>
2020-07-23 20:14:50 +0530

    enable usb debugging and netflix support, enable google assistant

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -5,3 +5,22 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Radio
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.radio.block_allow_data=1
+
+
+# USB debugging at boot
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mtp,adb \
+    ro.adb.secure=0 \
+    ro.secure=0 \
+    ro.debuggable=1
+
+# Assistant
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.opa.eligible_device=true
+
+# Apex
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.apex.updatable=true
+
+# Netflix
+ro.netflix.bsp_rev=Q845-05000-1
